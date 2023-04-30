c08377481c0c93ef27659abaa0ef96a3b05d0416
Natan <natan-moreira1@hotmail.com>
2023-04-30 17:33:12 -0300

    add count items to header and update IncreaseButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-// client component
 import Image from "next/image";
 import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
+import { useCoffeeStore } from "@/store/coffeeStore";
 import colors from "@/global/colors";
 import logo from "@/assets/logo.png";
 
 export function Header() {
+  const cart = useCoffeeStore((state) => state.cart);
+  const router = useRouter();
+
   return (
     <nav className="w-full max-w-6xl mx-auto px-3 py-8 flex justify-between">
-      <Image src={logo} alt="logo" className="w-[85px] h-[40px]" />
+      <Link href={"/"}>
+        <Image src={logo} alt="logo" className="w-[85px] h-[40px]" />
+      </Link>
 
       <div className="flex items-center gap-3">
         <button
@@ -20,8 +27,15 @@ export function Header() {
           Porto Alegre, RS
         </button>
 
-        <button className="rounded-md bg-yellow-light p-2 relative">
-          {/* <div className='absolute -top-2 -right-2 w-5 h-5 flex justify-center items-center bg-yellow-dark text-white font-bold text-xs rounded-full'>2</div> */}
+        <button
+          onClick={() => router.push("/checkout")}
+          className="rounded-md bg-yellow-light p-2 relative"
+        >
+          {cart.length > 0 && (
+            <div className="absolute -top-2 -right-2 w-5 h-5 flex justify-center items-center bg-yellow-dark text-white font-bold text-xs rounded-full">
+              {cart.length}
+            </div>
+          )}
           <ShoppingCart size={20} weight="fill" color={colors["yellow-dark"]} />
         </button>
       </div>
--- src/components/IncreaseButton/index.tsx
+++ src/components/IncreaseButton/index.tsx
@@ -2,15 +2,35 @@ import { Minus, Plus } from "@phosphor-icons/react";
 
 import colors from "@/global/colors";
 
-export function IncreaseButton() {
+interface IncreaseButtonProps {
+  add: () => void;
+  remove: () => void;
+  count: number;
+  isDisabledAdd?: boolean;
+  isDisabledRemove?: boolean;
+}
+
+export function IncreaseButton({
+  add,
+  remove,
+  count,
+  isDisabledAdd = false,
+  isDisabledRemove = false,
+}: IncreaseButtonProps) {
   return (
     <div className="p-1 flex items-center gap-2 rounded-md bg-base-button">
-      <button>
-        <Minus size={12} color={colors["purple-dark"]} />
+      <button onClick={remove} disabled={isDisabledRemove}>
+        <Minus
+          size={12}
+          color={isDisabledRemove ? colors["base-text"] : colors["purple-dark"]}
+        />
       </button>
-      <span className="text-base text-base-title">2</span>
-      <button>
-        <Plus size={12} color={colors["purple-dark"]} />
+      <span className="text-base text-base-title">{count}</span>
+      <button onClick={add} disabled={isDisabledAdd}>
+        <Plus
+          size={12}
+          color={isDisabledAdd ? colors["base-text"] : colors["purple-dark"]}
+        />
       </button>
     </div>
   );
