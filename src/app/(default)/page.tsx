8d7f317ff60882d94b0beca8df1ad8b540908e02
Natan <natan-moreira1@hotmail.com>
2023-05-09 23:01:23 -0300

    add server function to home page

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -2,7 +2,12 @@ import { CoffeeModel } from "@/domain/models/coffee";
 import { Home } from "@/pages/Home";
 import { prisma } from "@/services/prisma";
 
-export default async function Main() {
+async function getCoffeeData() {
   const data = (await prisma.coffee.findMany()) as CoffeeModel[];
+  return data;
+}
+
+export default async function Main() {
+  const data = await getCoffeeData();
   return <Home data={data} />;
 }
